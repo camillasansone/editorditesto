97f5190b17c0a3bdb14e104a83c4652840d9c80f
C.Sansone <98821748+camillasansone@users.noreply.github.com>
2022-03-23 00:30:33 +0100

    Delete scripts directory

--- scripts/FileDoc.js
+++
@@ -1,9 +0,0 @@
-export class FileDoc{
-    title="";
-    text="";
-    constructor(_title="", _text=""){
-        this.title = _title;
-        this.text = _text;
-    }
-}
-/* definizione della classe e costruttore: attenzione all'export del module per l'import nell'altro file */
--- scripts/UserInterface.js
+++
@@ -1,8 +0,0 @@
-export class UserInterface{
-    save='';
-    editor='';
-    file='';
-    title='';
-    new='';
-}
-/* qui c'è solo la definizione della classe*/
