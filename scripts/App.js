daa1dbc4039f707616025b597f904e9f6e22e9f1
C.Sansone <98821748+camillasansone@users.noreply.github.com>
2022-04-14 19:44:49 +0200

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -1,95 +1,90 @@
 import { FileDoc } from "./FileDoc.js";
 import { UserInterface } from "./UserInterface.js";
+
 /*importa le due classi definite negli altri files*/
-export class App {
-  /* costruttore: i dati sono presi dal file editor.js che contengono gli id dell'html, che istanzia una nuova app e tramite il costruttore assegna i valori all'istanza di UserInterface  */
-  constructor(_ui) {
-    this.ui = _ui;
 
-    /*inizializza tinymce, passando il riferimento all html tramite la proprietà dell'oggetto*/
-    tinymce.init({
-      selector: `#${this.ui.editor}`,
-    });
-    // quando nei metodi dovrò usare tinymce.get(this.ui.editor) avrò due metodi .setContent() e .getContent()
-    /*assegnare le proprietà dell'oggetto riferendosi al DOM e passando la proprietà dell'oggetto UserInterface come sopra*/
-    this.save = document.getElementById(`${this.ui.save}`);
-    this.title = document.getElementById(`${this.ui.title}`);
-    this.editor = document.getElementById(`${this.ui.editor}`);
-    this.file_list = document.getElementById(`${this.ui.file}`);
-    this.new = document.getElementById(`${this.ui.new}`);
+export class App{
+    ui = new UserInterface(); /* nuova istanza della classe UserInterface */
+    files = []; /* dichiara l'array files */
+    openFile = null; /* dichiara openFile e sotto idFile */
+    idFile = -1;
+    /* costruttore: i dati sono presi dal file editor.js che contengono gli id dell'html, che istanzia una nuova app e tramite il costruttore assegna i valori all'istanza di UserInterface  */
+    constructor(_ui){
+        this.ui = _ui;
 
-    /* chiamare il metodo che fa il bind dell'evento click */
-    this.getClick();
-
-    /* chiamare il metodo che recupera i dati dal localStorage*/
-    this.recoverText();
-  }
-
-  ui = new UserInterface();
-  files = []; // Array in cui tengo gli oggetti dei testi
-  openFile = null; // File aperto
-  textId = -1; // ID dei file iniziale
-
-  /* metodo che fa il bind sul click, attenzione all'uso di this*/
-  getClick() {
-    this.save.addEventListener("click", this.saveText.bind(this));
-    this.new.addEventListener("click", this.newText.bind(this));
-  }
-
-  /* metodo che recupera i dati nel localStorage*/
-  recoverText() {
-    if (localStorage.getItem("Texts")) {
-      this.files = JSON.parse(localStorage.getItem("Texts"));
-      this.buildList();
-    }
-  }
-
-  /* metodo che carica l'oggetto file */
-  showText(listItem) {
-    this.textId = listItem.target.dataset.id;
-    this.openFile = new FileDoc(
-      this.files[this.textId].title,
-      this.files[this.textId].text
-    );
-    this.title.value = this.openFile.title;
-    tinymce.get(this.ui.editor).setContent(this.openFile.text);
-  }
-
-  /* metodo che ripulisce */
-  newText() {
-    this.title.value = "";
-    tinymce.get(this.ui.editor).setContent("");
-    this.openFile = null;
-  }
-
-  /* altro metodo: se non ci sono file caricati crea un oggetto file e fa il push nell'array */
-  /* altrimenti modifica il file assegnando i valori letti dal form*/
-  saveText() {
-    if (this.openFile == null && this.title.value != "") {
-      let doc = new FileDoc();
-      doc.title = this.title.value;
-      doc.text = tinymce.get(this.ui.editor).getContent();
-      this.files.push(doc);
-    } else if (this.openFile != null && this.title.value != "") {
-      this.openFile.title = this.title.value;
-      this.openFile.text = tinymce.get(this.ui.editor).getContent();
-      this.files[this.textId] = this.openFile;
-    } else {
-      this.files.splice(this.textId, 1); // Elimina il file in caso non ci siano presenti titoli
-    }
-    console.log("Testo Salvato");
-    localStorage.setItem("Texts", JSON.stringify(this.files));
-    this.buildList();
-    this.newText();
-  }
-  /* salva l'array nel localStorage e chiama la funzione che stampa a video*/
-
-  /* metodo che stampa a video */
-  buildList() {
-    this.file_list.innerHTML = "";
-        this.files.forEach((element, index) => {
-            this.file_list.innerHTML += (`<li data-id="${index}" class="list-group-item testi">${element.title}</li>`)
+        /*inizializza tinymce, passando il riferimento all html tramite la proprietà dell'oggetto*/
+        tinymce.init({
+            selector: `#${this.ui.editor}`
         });
-        $(".testi").click(this.showText.bind(this));
+
+        /*assegna le proprietà dell'oggetto riferendosi al DOM e passando la proprietà dell'oggetto UserInterface come sopra*/
+        this.save = document.querySelector(`#${this.ui.save}`);
+        this.title = document.querySelector(`#${this.ui.title}`);
+        this.editor = document.querySelector(`#${this.ui.editor}`);
+        this.file_list = document.querySelector(`#${this.ui.file}`);
+        this.new = document.querySelector(`#${this.ui.new}`);
+        /* chiama il metodo definito sotto che fa il bind dell'evento click */
+        this.eventHandlers();
+        /* chiama il metodo loadDocs che recupera i dati dal localStorage*/
+        this.loadDocs();
+    }
+    eventHandlers(){
+    /*attenzione al bind this: è necessario passare il this con bind altrimenti risulterebbe undefined all'interno della funzione: cioè tu non "vedi" this all'interno della funzione e quindi lo devi ri-passare tramite bind */
+        this.save.addEventListener("click", this.saveDoc.bind(this));
+        this.new.addEventListener("click", this.newDoc.bind(this));
+    }
+    loadDocs(){
+        if(localStorage.getItem("files")){
+            this.files = JSON.parse(localStorage.getItem("files"));
+            /*legge il localStorage e poi chiama il metodo build*/
+            this.build();
+        }
+    }
+    loadFile(el){
+    /*metodo che carica l'oggetto file */
+        this.idFile = el.target.dataset.id;
+        this.openFile = new FileDoc(this.files[this.idFile].title, this.files[this.idFile].text);
+        this.title.value =this.openFile.title;
+        tinymce.get(this.ui.editor).setContent(this.openFile.text);
+    }
+    newDoc(){
+        /*chiamata al metodo per ripulire */
+        this.clean();
+    }
+    saveDoc(){
+    /*se openFile è vuoto crea un nuovo oggetto file e fa il push nell'array files*/
+        if(this.openFile == null){
+            let file = new FileDoc();
+            file.title = this.title.value;
+            file.text = tinymce.get(this.ui.editor).getContent();
+            this.files.push(file);
+        }else{
+        /*altrimenti modifica il file terza riga assegnando i valori letti dal form*/
+            this.openFile.title = this.title.value;
+            this.openFile.text = tinymce.get(this.ui.editor).getContent();
+            this.files[this.idFile] = this.openFile;
+        }
+       // salva l'array files nel localstorage
+        localStorage.setItem("files", JSON.stringify(this.files));
+       // chiama la funzione che stampa a video
+        this.build();
+
+    }
+    build(){
+    /*pulisce il form*/
+        this.clean();
+        /*pulisce il contenitore dei risultati*/
+        this.file_list.innerHTML = "";
+        /*con il ciclo foreach appende la stringa html nel contenitore in pagina*/
+        this.files.forEach((el, index) => {
+            this.file_list.innerHTML += (`<li data-id="${index}" class="list-group-item list-group-item-action docs">${el.title}</li>`)
+        });
+        $(".docs").click(this.loadFile.bind(this));
+    }
+    clean(){
+    /* svuota il form e annulla openFile*/
+        this.title.value = "";
+        tinymce.get(this.ui.editor).setContent('');
+        this.openFile = null;
     }
 }
